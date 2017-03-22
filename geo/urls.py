400ef2943a92b372be67c626292e99596789983f
hvoking <gustavoinge@hotmail.com>
2017-03-22 05:25:47 -0300

    'mapa2'

--- geo/urls.py
+++ geo/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^maps?apikey=<insert-your-apikey-here>/$', views.maps, name='mapa'),
+    url(r'^maps/$', views.maps, name='mapa'),
     url(r'^mapa2/$', views.mapa2, name='mapa2'),
     
 ]
