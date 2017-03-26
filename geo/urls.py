2a8342dc9141c6eaeb693897ee9539f1246d1241
hvoking <gustavoinge@hotmail.com>
2017-03-26 17:40:29 -0300

    'changesmap'

--- geo/urls.py
+++ geo/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^maps/$', views.maps, name='mapa'),
     url(r'^mapa2/$', views.mapa2, name='mapa2'),
-    url(r'^mapa3/$', views.mapa2, name='mapa3'),
     url(r'^mapa3d/$', views.mapa3d, name='mapa3d'),
+
 ]
--- geo/views.py
+++ geo/views.py
@@ -2,14 +2,11 @@ from django.shortcuts import render
 from django.template.response import TemplateResponse
 
 
-def maps(request):
-	return TemplateResponse (request, 'mapa.html', {})
-
 def mapa2(request):
 	return TemplateResponse (request, 'mapa2.html', {})
 
-def mapa3(request):
-	return TemplateResponse (request, 'mapa3.html', {})	
-
 def mapa3d(request):
-	return TemplateResponse (request, 'mapa3d.html', {})	
+	return TemplateResponse (request, 'mapa3d.html', {})
+
+def mapa4d(request):
+	return TemplateResponse (request, 'mapa4d.html', {})	
